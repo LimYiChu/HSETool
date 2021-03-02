a5d617998f510cd02c3e0d55ea867338bd03acf6
emdr07 <marcel.emdr@gmail.com>
2021-03-02 17:49:12 +0800

    test

--- UploadExcel/models.py
+++ UploadExcel/models.py
@@ -17,7 +17,7 @@ class ActionItems(models.Model):
     Facility        =   models.CharField(max_length=255,null=True)
     ProjectPhase           =   models.CharField(max_length=255,null=True)
     Cause           =   models.TextField(null=True)
-    
+    SafeG           = models.TextField(null=True)
     Consequence     =   models.TextField(null=True)
     Recomendations  =   models.TextField(null=True)
     InitialRisk     =   models.CharField(max_length=10,null=True)
