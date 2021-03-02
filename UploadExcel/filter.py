c1d87353ba2a0499757e7fb42044e39c488cbd15
emdr07 <79830113+emdr07@users.noreply.github.com>
2021-03-02 15:01:37 +0800

    Delete UploadExcel directory

--- UploadExcel/filter.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-
-from .models import *
-
-class ActionItemsFilter(django_filters.FilterSet):
-    class Meta:
-        model= ActionItems
-        fields= '__all__'
--- UploadExcel/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UploadExl
-
-class UploadExlForm(forms.ModelForm):
-    class Meta:
-        model = UploadExl
-        fields = '__all__'
