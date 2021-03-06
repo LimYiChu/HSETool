c66a04310b60d4c786d7dcdbcda5bdd1bb672687
yhsiang2001 <yhsiang@gmail.com>
2021-03-06 16:30:30 +0800

    on the way

--- Trackem/urls.py
+++ Trackem/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
         path('password_change/',auth_views.PasswordChangeView.as_view(template_name='userT/password_change.html'),name='password_change'),
         path('password_reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='userT/password_reset_done.html'),name='password_reset_done'),
         path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-        path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+        path('password_reset/',auth_views.PasswordResetView.as_view(template_name='userT/password_reset_form.html'),name='password_reset'),
         path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='userT/password_reset_complete.html'),name='password_reset_complete'),
       ]
