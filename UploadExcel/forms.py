f11bbd26fd08b91e47469e41ea25f05aefcc3eef
gunav <gunaselanv@gmail.com>
2021-03-16 07:12:07 +0800

    UpdateView

--- UploadExcel/forms.py
+++ UploadExcel/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import UploadExl
-
+from .models import *
+from crispy_forms.helper import FormHelper
 class UploadExlForm(forms.ModelForm):
     class Meta:
         model = UploadExl
         fields = '__all__'
+
+class UpdateActioneeForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
+    class Meta:
+        model = ActionItems
+        fields = '__all__'
+
+        StudyActionNo = forms.CharField(disabled=True)
--- UploadExcel/models.py
+++ UploadExcel/models.py
@@ -39,3 +39,6 @@ class ActionItems(models.Model):
 
     def get_absolute_url(self):
         return reverse("getActionsDetails", kwargs={"id": self.id})
+
+    def __str__(self): 
+        return self.StudyActionNo   
