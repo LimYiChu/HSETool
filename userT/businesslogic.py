9824a92b5da6b742ab39cd0882f3e66f524ed6b3
gunav <gunaselanv@gmail.com>
2021-03-21 07:15:09 +0800

    untilDetail

--- userT/businesslogic.py
+++ userT/businesslogic.py
@@ -54,6 +54,34 @@ def blfuncActionCount(contextRoutes,que):
     #return ActionItems.ActioneeItems.get_myItemsbyCompDisSub(blvarorganisation,blvardisipline,blvarSUbdisipline)
     return [ firststream,  secondstream,  thirdstream]
 
+def blfuncgetallAction(contextRoutes,que):
+   #This functionality already works
+   #Initilises count in case 
+    firststream = 0
+    secondstream = 0
+    thirdstream = 0
+    
+    for x, item in enumerate(contextRoutes):
+        blvarorganisation   = item.Organisation
+        blvardisipline  = item.Disipline
+        blvarSUbdisipline  = item.Subdisipline
+        blque               =   que
+        if x==0:
+            firststream = ActionItems.myActionItemsCount.get_myItemsCount(blvarorganisation,
+                                                                blvardisipline,
+                                                                blvarSUbdisipline,blque)
+        if x==1:
+            secondstream = ActionItems.myActionItemsCount.get_myItemsCount(blvarorganisation,
+                                                                blvardisipline,
+                                                                blvarSUbdisipline,blque)
+        if x==2:
+            thirdstream =  ActionItems.myActionItemsCount.get_myItemsCount(blvarorganisation,
+                                                                blvardisipline,
+                                                                blvarSUbdisipline,blque)
+    #return ActionItems.ActioneeItems.get_myItemsbyCompDisSub(blvarorganisation,blvardisipline,blvarSUbdisipline)
+    return [ firststream,  secondstream,  thirdstream]
+
+
 def blgetActioneeItemsbyStream(contextRoutes,stream): 
     que = 0 #denotes actionee items
     firststream = []
