e0a9646c6f17247a11ad91f495eb73802484c902
emdr07 <marcel.emdr@gmail.com>
2021-04-24 01:31:01 +0800

    tenant 1

---
+++ Tenant/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Tenant(models.Model):
+    name = models.CharField(max_length=200)
+    Subdomain = models.CharField(max_length=200)
+
+class TenantAwareModel(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+class Member(TenantAwareModel):
+    name = models.CharField(max_length=200)
