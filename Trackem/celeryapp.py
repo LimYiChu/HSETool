7d914877d585cd50a34fc478ec58411156024d0c
yhsiang2001 <yhsiang@gmail.com>
2021-04-24 10:52:23 +0800

    ok

--- Trackem/celeryapp.py
+++ Trackem/celeryapp.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trackem.settings')
 from django.conf import settings
-app = Celery('project')
+app = Celery('Trackem')
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 app.config_from_object('django.conf:settings')
--- static/Javascript/js.js
+++ static/Javascript/js.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#example thead tr').clone(true).appendTo( '#example thead' );
     $('#example thead tr:eq(1) th').each( function (i) {
         var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        $(this).html( '<input type="text" placeholder="Search" />' );
  
         $( 'input', this ).on( 'keyup change', function () {
             if ( table.column(i).search() !== this.value ) {
@@ -19,7 +19,7 @@ $(document).ready(function() {
     var table = $('#example').DataTable( {
         orderCellsTop: true,
         fixedHeader: true,
-         "lengthMenu": [[-1, 10, 25, 50, 100], ["All", 10, 25, 50, 100]]
+         "lengthMenu": [[-1, 10, 25, 50, 100, 200], ["All", 10, 25, 50, 100, 200]]
     } );
 } );
 
