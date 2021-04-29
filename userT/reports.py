6b03bec44cfbcb703e47552bfbcf54017664f6b2
gunav <46297843+gunav@users.noreply.github.com>
2021-04-29 19:51:27 +0800

    TimeStampWip

--- userT/reports.py
+++ userT/reports.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import base64
+import math
 from io import BytesIO
 import numpy as np
 
@@ -14,7 +16,7 @@ def get_graph():
     buffer.close()
     return graph
 
-def showPie(dataAct,labelsAct,Title):
+def showPie(dataAct,labelsAct,title):
     plt.switch_backend('AGG')
     #plt.style.use("fivethirtyeight") #- used in Coreyvideo, not sure why its better but leave it first
     
@@ -38,15 +40,17 @@ def showPie(dataAct,labelsAct,Title):
         return "{:.0f} %\n({:d})".format(pct, absolute) #"{:.0f} %\n({:d} Actions)".format(pct, absolute)
 
     fig = plt.figure()
-    fig.suptitle(Title, fontsize=21, fontweight='bold', wrap=True)
-    plt.rcParams['font.size'] = 18.0
+    fig.suptitle(title, fontsize=21, fontweight='bold', wrap=True)
+    #plt.title (title,fontsize=20, fontweight='bold', wrap=True)
+    plt.rcParams['font.size'] = 15.0
     plt.rcParams['font.weight'] = "bold"
+    plt.rcParams["figure.figsize"] = (6,6)
     #strdisplay = '%.1f %%'
     #strdisplay ='%d'
     #lamda is just an inline function - nothing fancy
     plt.pie(data,colors=colors, radius = radius, startangle = 90,  explode = explodeslice, shadow=True,autopct=lambda pct: func(pct, data), wedgeprops ={'edgecolor':'black'})
 
-    plt.legend(labels, bbox_to_anchor=(1,1), loc="upper left")
+    plt.legend(labels, bbox_to_anchor=(0.82,1.0), loc="upper left")
     #plt.title (Title)
     plt.tight_layout()
     
@@ -67,14 +71,33 @@ def showbar (listcountbyDisSub,totalcountbyDisSub,listlablebyDisSub, label1,labe
 
     data_y_axes = listcountbyDisSub
     labels_x_axes =listlablebyDisSub
+    
+    maxcount = max(totalcountbyDisSub) #the following is to adjust the y axes ranges
+    stepcount = 1
+    if maxcount >= 5:
+
+        stepcount = math.ceil(maxcount/5)
+        print(stepcount)
     x_indexes = np.arange(len(labels_x_axes))
+    #y_indexes = np.arange(0, maxcount,stepcount)
+    #y_indexes = np.arange(0, 2,0.5)
     width=0.25
     #x_index
     plt.bar(x_indexes, data_y_axes, label=label1, width=width, color="#91CACC")
     plt.bar(x_indexes-width, totalcountbyDisSub, label=label2, width=width, color="#5E6565")
+    
+    #fig = plt.figure()
+    #fig.suptitle(title, fontsize=15, fontweight='bold', wrap=True)
+    #plt.rcParams['font.size'] = 15.0
+    #plt.rcParams['font.weight'] = "bold"
+    plt.rcParams["figure.figsize"] = (8,10)
+    plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
+
     plt.legend()
-    plt.title (title)
-    plt.xticks (ticks=x_indexes-(width/2) , labels=labels_x_axes)
+    plt.title (title,fontsize=20, fontweight='bold', wrap=True)
+    plt.xticks (ticks=x_indexes-(width/2) , labels=labels_x_axes, fontsize='12',fontweight='bold')
+    #plt.yticks(y_indexes)
+    #plt.gca().yax
     plt.xlabel (generalxlabel)
     plt.tight_layout()
     graph =get_graph()
