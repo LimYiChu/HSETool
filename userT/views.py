18e5cbd1bcb5f2adca01b1fdf863a24f053e21dd
emdr07 <marcel.emdr@gmail.com>
2021-05-21 08:11:51 +0800

    email all final comments and overdue button

--- userT/views.py
+++ userT/views.py
@@ -849,23 +849,18 @@ def emailreminders(request):
         discsuborg = ActionRoutes.mdlAllDiscSub.mgr_getDiscSubOrg() #get all disc sub
         Indisets = blgetIndiResponseCount(discsuborg,QueOpen,QueClosed)   
         subject = "Pending Actions"
-        content="You have Pending Actions in your Queue. Please go to https://prism.prism-ehstools.com/ to attend to the actions." 
+        content="You have Pending Actions in your Queue. Please go to https://sapuraphase4a.prism-ehstools.com/ to attend to the actions." 
         for items in Indisets : 
             if items[3]>0:
                 emaillist.append(items[0])
         blemailSendindividual(emailSender,emaillist,subject,content)
-        #below is for the overdue, it is linked to button, just waiting for overview function
+        #below is for the overdue, it is linked to button, just waiting for overdue function
     elif (request.POST.get('SendOverdue')):
-        QueOpen = [0,1,2,3,4,5,6,7,8,9]
-        QueClosed = [99]
-        discsuborg = ActionRoutes.mdlAllDiscSub.mgr_getDiscSubOrg() #get all disc sub
-        Indisets = blgetIndiResponseCount(discsuborg,QueOpen,QueClosed)   
+          
         subject = "Pending Actions"
-        content="You have Overdue Actions in your Queue. Please go to https://prism.prism-ehstools.com/ to attend to the actions." 
-        for items in Indisets : 
-            if items[3]>0:
-                emaillist.append(items[0])
+        content="You have Overdue Actions in your Queue. Please go to https://sapuraphase4a.prism-ehstools.com/ to attend to the actions." 
         blemailSendindividual(emailSender,emaillist,subject,content)
+
         return render (request, 'userT/emailreminders.html')
     return render (request, 'userT/emailreminders.html')
     
