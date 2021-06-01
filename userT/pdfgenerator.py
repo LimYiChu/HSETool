d085a72a602ba02b8c0430c73aa4d43ea8deed75
emdr07 <marcel.emdr@gmail.com>
2021-06-01 08:17:17 +0800

    pdf changes

--- userT/pdfgenerator.py
+++ userT/pdfgenerator.py
@@ -5,14 +5,16 @@ def pdfgenerate(input_pdf_path, output_pdf_path, data_dict,signatories):
     template_pdf = pdfrw.PdfReader(input_pdf_path) # Read Input PDF
     template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))# Set Appearences ( Make Text field visible )
     # Loop all Annotations
-    for annotation in template_pdf.pages[0]['/Annots']:#loop over all pages in pdf, and for each page, loop over all ediatble fields called annots, for each page all annotations stored in 'Annots/' 
-        if annotation['/Subtype'] == '/Widget' and annotation['/T']:  # all key names for each field stored in'/T'
-            key = annotation['/T'][1:-1] # Remove parentheses
-            if key in data_dict.keys():
-                annotation.update( pdfrw.PdfDict(V=f'{data_dict[key]}')) #takes the data from models with [key], printing key returns all key field from models (along with pdf details such as font type,etc)
-            if key in signatories.keys():
-                annotation.update( pdfrw.PdfDict(V=f'{signatories[key]}'))
-    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+    for page in template_pdf.pages: #[0]['/Annots']:#loop over all pages in pdf, and for each page, loop over all ediatble fields called annots, for each page all annotations stored in 'Annots/' 
+        annotations=page['/Annots']
+        for annotation in annotations:
+            if annotation['/Subtype'] == '/Widget' and annotation['/T']:  # all key names for each field stored in'/T'
+                key = annotation['/T'][1:-1] # Remove parentheses
+                if key in data_dict.keys():
+                    annotation.update( pdfrw.PdfDict(V=f'{data_dict[key]}')) #takes the data from models with [key], printing key returns all key field from models (along with pdf details such as font type,etc)
+                if key in signatories.keys():
+                    annotation.update( pdfrw.PdfDict(V=f'{signatories[key]}'))
+        pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
 
 
 def pdfsendtoclient(input_pdf_path, data_dict):
@@ -22,12 +24,14 @@ def pdfsendtoclient(input_pdf_path, data_dict):
     template_pdf = pdfrw.PdfReader(input_pdf_path) # Read Input PDF
     template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))# Set Appearences ( Make Text field visible )
     # Loop all Annotations
-    for annotation in template_pdf.pages[0]['/Annots']:#loop over all pages in pdf, and for each page, loop over all ediatble fields called annots, for each page all annotations stored in 'Annots/' 
-        if annotation['/Subtype'] == '/Widget' and annotation['/T']:  # all key names for each field stored in'/T'
-            key = annotation['/T'][1:-1] # Remove parentheses
-            if key in data_dict.keys():
-                page = annotation.update( pdfrw.PdfDict(V=f'{data_dict[key]}')) #takes the data from models with [key], printing key returns all key field from models (along with pdf details such as font type,etc)
-    
+    for page in template_pdf.pages: #[0]['/Annots']:#loop over all pages in pdf, and for each page, loop over all ediatble fields called annots, for each page all annotations stored in 'Annots/' 
+        annotations=page['/Annots']
+        for annotation in annotations:
+            if annotation['/Subtype'] == '/Widget' and annotation['/T']:  # all key names for each field stored in'/T'
+                key = annotation['/T'][1:-1] # Remove parentheses
+                if key in data_dict.keys():
+                    page = annotation.update( pdfrw.PdfDict(V=f'{data_dict[key]}')) #takes the data from models with [key], printing key returns all key field from models (along with pdf details such as font type,etc)
+        
     pdfrw.PdfWriter().write(buffer, template_pdf)
     buffer.seek(0)
 
