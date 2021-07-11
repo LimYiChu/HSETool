d682413088c70942a133be2a0e96a503c7fd679a
emdr07 <marcel.emdr@gmail.com>
2021-07-11 17:21:14 +0800

    test migrations

--- Trackem/settings.py
+++ Trackem/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Trackem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'trackemupgrade',
+        'NAME': 'trackem',
         'USER': 'root',
         'PASSWORD': 'Nice10day',
         'HOST': 'localhost',
