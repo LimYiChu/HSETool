87e05b2e97a79a734ab46fdedd8c033161d30409
ishna-m <ishna.maanishi@gmail.com>
2022-03-18 12:53:45 +0800

    button and font size

--- static/Javascript/dynamictable.js
+++ static/Javascript/dynamictable.js
@@ -12,12 +12,12 @@ function fadein(that) {
 }
 
 var title;
+
 function ajaxcall(event, tablepopup) {
   var data = event.currentTarget.firstElementChild.innerText
   title = event.currentTarget.firstElementChild.innerText
   $('.caption').html(title);
-  $("input.hidden").val(title);
-//   alert($("input.hidden").val)
+
   $.ajax({
       url: "/" + tablepopup,
       type: 'GET',
@@ -32,106 +32,83 @@ function ajaxcall(event, tablepopup) {
           dynamictabledisc(response, this.rootid);
       }
   })
-
-  
-}
-
-function alerttest() {
-    data = title
-    // alert(data)
-
-    $.ajax({
-        url: "/dynamicstudiesexceldisc" ,
-        type: 'GET',
-        data: {
-            "data": data
-        },
-        success: function(response) {
-           alert(response)
-        }
-    })
 }
 
 function dynamictable(response,rootid) {
 
-  var dflist = response.dflist
-  var headerlist = response.headerlist
+    var dflist = response.dflist
+    var headerlist = response.headerlist
 
-  var dynamicheaders = [];
-  for (var i = 0; i < headerlist.length; i++) 
-  {
-      dynamicheaders.push({
-          "sTitle": headerlist[i],
-      });
-  };
-
-    // var tables = $('table.dynamictable');
-    // for(var i=0; i<tables.length; i++){
-    //     var table = tables.eq(i);
-    //     var tableid = rootid + 'table' + i
-    //     table.attr('id',tableid);
-    // }
-  
-  $('table.dynamictable').each(function() {
-
-    var datatableid = '#' + $(this).attr('id');
+    var dynamicheaders = [];
+    for (var i = 0; i < headerlist.length; i++) 
+    {
+        dynamicheaders.push({
+            "sTitle": headerlist[i],
+        });
+    };
     
-      if ($.fn.dataTable.isDataTable(datatableid)) 
-      {
-          $(datatableid).DataTable().destroy();
-          $(datatableid).empty();
-      }
+    $('table.dynamictable').each(function() 
+    {
+        var datatableid = '#' + $(this).attr('id');
+        
+        if ($.fn.dataTable.isDataTable(datatableid)) 
+        {
+            $(datatableid).DataTable().destroy();
+            $(datatableid).empty();
+        }
 
-      var table = $(datatableid).DataTable({
-          data: dflist,
+        var table = $(datatableid).DataTable(
+        {
+            data: dflist,
 
-          "aoColumns": dynamicheaders,
-          "columnDefs": 
-          [{
-              "targets": [0],
-              "mRender": function(data, row, column) {
-                  return '<a href="/pmtrepviewall' + '/' + column[5] + '/view' + '">' + data + '</a>';
-              }
-          }, ],
-          "bDestroy": true,
-          orderCellsTop: true,
-          "lengthMenu": 
-          [
-              [-1, 10, 25, 50, 200],
-              ["All", 10, 25, 50, 200]
-          ],
-          "createdRow": function(row, data, dataIndex, column) 
-          {
-              if (data[6] == 'Red') 
-              {
-                return $(column[4]).addClass('table-danger')
-              } 
-              else if (data[6] == 'Yellow') 
-              {
-                return $(column[4]).addClass('table-warning')
-              } 
-              else if (data[6] == 'Green') 
-              {
-                return $(column[4]).addClass('table-success');
-              }
-          },
-      });
+            "aoColumns": dynamicheaders,
+            "columnDefs": 
+            [{
+                "targets": [0],
+                "mRender": function(data, row, column) {
+                    return '<a href="/pmtrepviewall' + '/' + column[5] + '/view' + '">' + data + '</a>';
+                }
+            }, ],
+            "bDestroy": true,
+            orderCellsTop: true,
+            "lengthMenu": 
+            [
+                [-1, 10, 25, 50, 200],
+                ["All", 10, 25, 50, 200]
+            ],
+            "createdRow": function(row, data, dataIndex, column) 
+            {
+                if (data[6] == 'Red') 
+                {
+                    return $(column[4]).addClass('table-danger')
+                } 
+                else if (data[6] == 'Yellow') 
+                {
+                    return $(column[4]).addClass('table-warning')
+                } 
+                else if (data[6] == 'Green') 
+                {
+                    return $(column[4]).addClass('table-success');
+                }
+            },
+        });
 
-      $(datatableid + ' thead tr').clone(false).appendTo(datatableid + ' thead'); //clone headers
-      $(datatableid + ' thead tr:eq(1) th').each(function(i) {
-          $(this).html('<input type="text" placeholder="Search" class="column_search" />');
-          $(this).removeClass("sorting sorting_asc sorting_desc")
-      });
+        $(datatableid + ' thead tr').clone(false).appendTo(datatableid + ' thead'); //clone headers
+        $(datatableid + ' thead tr:eq(1) th').each(function(i) {
+            $(this).html('<input type="text" placeholder="Search" class="column_search" />');
+            $(this).removeClass("sorting sorting_asc sorting_desc")
+        });
 
-      $(datatableid + ' thead').on('keyup', ".column_search", function() {
-          table
-              .column($(this).parent().index())
-              .search(this.value)
-              .draw();
-      });
-  });
+        $(datatableid + ' thead').on('keyup', ".column_search", function() {
+            table
+                .column($(this).parent().index())
+                .search(this.value)
+                .draw();
+        });
+    });
 
-  $('#buttonplace').html(" <a class='article-title' href='/dynamicstudiesexceldisc/" + title + "' >Download Excel</a>")           
+    $('#buttonplace').html(" <a class='article-title' href='/dynamicstudiesexceldisc/" + title + 
+    "' ><button type='button' class='btn btn-outline-primary btn-md;' style='width:150pt'>Download Excel</button></a>")           
 }
 
 function fadeout(that) {
@@ -205,6 +182,13 @@ function drawPieChart (value,index,rootid,singlediv) {
     );
     var options = {
         title:newtitle,
+        titleTextStyle: 
+        {
+            color: 'black',
+            fontSize: 16,
+            bold: true,  
+            italic: false,
+        },
         width: 350,
         height: 270,
         backgroundColor: '#f3f2f2',
@@ -241,32 +225,8 @@ function drawPieChart (value,index,rootid,singlediv) {
         }
     };
 
-    // var chartid = rootid + 'chart'
-    // $('div.dynamicchart').attr('id',chartid)
-
-    // var charts = $('div.dynamicchart');
-    // for(var i=0; i<charts.length; i++){
-    //     var chart = charts.eq(i);
-    //     var chartid = rootid + 'chart' + index
-    //     $("<div/>").attr('id',chartid).appendTo(chart);
-    // }
-
-    // var $div = $('.dynamicchart')
-    // id = chartid + index
-    // $("<div/>").attr('id',chartid).appendTo($div);
-
-    // var $div2 = $('.dynamicchart2')
-    // index2 = parseInt(index) + 2
-    // id2 = 'chart'+ index2
-    // $("<div/>").attr('id',id2).appendTo($div2);
-   
-    // var finalpie = chartid.concat(index);
-    // var chart = new google.visualization.PieChart(document.getElementById(finalpie));
-    // chart.draw(data, options);
     var chart = new google.visualization.PieChart(singlediv);
     chart.draw(data, options);chart.draw(data, options);
-    // var chart2 = new google.visualization.PieChart(document.getElementById(id2));
-    // chart2.draw(data, options);
 }
 
 function openTab(evt, tabname) {
@@ -284,79 +244,81 @@ function openTab(evt, tabname) {
     }
     $(document).ready(function() {
     document.getElementById("defaultOpen").click();  
-    })
+})
 
 
 function dynamictabledisc(response,rootid) {
    
-        var discheaderlst = response.discheaderlst
-        var disclst = response.disclst
-      
-        var dynamicheaders = [];
-        for (var i = 0; i < discheaderlst.length; i++) 
-        {
-            dynamicheaders.push({
-                "sTitle": discheaderlst[i],
-            });
-        };
-      
-          var tables = $('table.dynamictabledisc');
-          for(var i=0; i<tables.length; i++){
-              var table = tables.eq(i);
-              var tableid = rootid + 'table' + i
-              table.attr('id',tableid);
-          }
-        
-        $('table.dynamictabledisc').each(function() {
-      
-          var datatableid = '#' + $(this).attr('id');
-          
-            if ($.fn.dataTable.isDataTable(datatableid)) 
-            {
-                $(datatableid).DataTable().destroy();
-                $(datatableid).empty();
-            }
-      
-            var table = $(datatableid).DataTable({
-                data: disclst,
-      
-                "aoColumns": dynamicheaders,
-                
-                "bDestroy": true,
-                orderCellsTop: true,
-                "lengthMenu": 
-                [
-                    [-1, 10, 25, 50, 200],
-                    ["All", 10, 25, 50, 200]
-                ],
-                "createdRow": function(row, data, dataIndex, column) 
-                {
-                    if (data[6] == 'Red') 
-                    {
-                      return $(column[4]).addClass('table-danger')
-                    } 
-                    else if (data[6] == 'Yellow') 
-                    {
-                      return $(column[4]).addClass('table-warning')
-                    } 
-                    else if (data[6] == 'Green') 
-                    {
-                      return $(column[4]).addClass('table-success');
-                    }
-                },
-            });
-      
-            $(datatableid + ' thead tr').clone(false).appendTo(datatableid + ' thead'); //clone headers
-            $(datatableid + ' thead tr:eq(1) th').each(function(i) {
-                $(this).html('<input type="text" placeholder="Search" class="column_search" />');
-                $(this).removeClass("sorting sorting_asc sorting_desc")
-            });
-      
-            $(datatableid + ' thead').on('keyup', ".column_search", function() {
-                table
-                    .column($(this).parent().index())
-                    .search(this.value)
-                    .draw();
-            });
+    var discheaderlst = response.discheaderlst
+    var disclst = response.disclst
+    
+    var dynamicheaders = [];
+    for (var i = 0; i < discheaderlst.length; i++) 
+    {
+        dynamicheaders.push({
+            "sTitle": discheaderlst[i],
         });
-      }
+    };
+    
+    var tables = $('table.dynamictabledisc');
+    for(var i=0; i<tables.length; i++)
+    {
+        var table = tables.eq(i);
+        var tableid = rootid + 'table' + i
+        table.attr('id',tableid);
+    }
+        
+    $('table.dynamictabledisc').each(function() 
+    {
+      
+        var datatableid = '#' + $(this).attr('id');
+        
+        if ($.fn.dataTable.isDataTable(datatableid)) 
+        {
+            $(datatableid).DataTable().destroy();
+            $(datatableid).empty();
+        }
+    
+        var table = $(datatableid).DataTable({
+            data: disclst,
+    
+            "aoColumns": dynamicheaders,
+            
+            "bDestroy": true,
+            orderCellsTop: true,
+            "lengthMenu": 
+            [
+                [-1, 10, 25, 50, 200],
+                ["All", 10, 25, 50, 200]
+            ],
+            "createdRow": function(row, data, dataIndex, column) 
+            {
+                if (data[6] == 'Red') 
+                {
+                    return $(column[4]).addClass('table-danger')
+                } 
+                else if (data[6] == 'Yellow') 
+                {
+                    return $(column[4]).addClass('table-warning')
+                } 
+                else if (data[6] == 'Green') 
+                {
+                    return $(column[4]).addClass('table-success');
+                }
+            },
+        });
+      
+        $(datatableid + ' thead tr').clone(false).appendTo(datatableid + ' thead'); //clone headers
+        $(datatableid + ' thead tr:eq(1) th').each(function(i) {
+            $(this).html('<input type="text" placeholder="Search" class="column_search" />');
+            $(this).removeClass("sorting sorting_asc sorting_desc")
+        });
+    
+        $(datatableid + ' thead').on('keyup', ".column_search", function() {
+            table
+                .column($(this).parent().index())
+                .search(this.value)
+                .draw();
+        });
+    });
+}
